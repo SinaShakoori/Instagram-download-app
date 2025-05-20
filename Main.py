d8ca176d95d1fa89ed07f7b44946ebbcedcbd99e
Sinashakoori <Sinashakoori79@gmail.com>
2025-05-20 21:03:27 +0330

    Config file

--- Main.py
+++ Main.py
@@ -3,28 +3,51 @@ from selenium.webdriver.chrome.service import Service as chromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time 
+from Config import username,password
 
-Service = chromeService(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=Service)
+try:
+    Service = chromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=Service)
+
+    driver.get("https://www.instagram.com/")
+    rob = driver.find_element(By.XPATH,"""/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]""")
+    rob.click()
+    time.sleep(3)
+    user = driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[1]/div/label/input""")
+    user.send_keys(username)
+    time.sleep(2)
+    passw= driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[2]/div/label/input""")
+    passw.send_keys(password)
+    time.sleep(2)
+    log = driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[3]/button/div""")
+    log.click()
+    time.sleep(10)
+    try:
+        
+        save = driver.find_element(By.XPATH,"""//*[@id="mount_0_0_lM"]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div/div/div""")
+        save.click()
+        time.sleep(5)
+    except:
+        print(f"Error occurred: سیو!")
+
+    try:
+        
+        driver.get("https://www.instagram.com/50cent/?g=5")
+        time.sleep(5)
+        post = driver.find_element(By.XPATH,"""//*[@id="mount_0_0_Lo"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/div[2]/div/div/div/div[1]/div[1]/a/div[2]""")
+        post.click()
+        time.sleep(2)
+        like = driver.find_element(By.XPATH,"""/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/div/div/div""")
+        like.click()
+        time.sleep(2)
+        back = driver.find_element(By.XPATH,"""/html/body/div[6]/div[1]/div/div[2]/div/div/svg""")
+        back.click()
+    except:
+        print ( "مشکل لایک!") 
 
 
-driver.get("https://www.instagram.com/")
-
-
-username = "username"
-password = "123456789"
-link=""
-user = driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[1]/div/label/input""")
-user.send_keys(f"{username}")
-
-passw= driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[2]/div/label/input""")
-passw.send_keys(f"{password}")
-
-
-log = driver.find_element(By.XPATH,"""//*[@id="loginForm"]/div[1]/div[3]/button""")
-log.click()
-
-
-
-
-time.sleep(10)
+except Exception as e:
+    print(f"Error occurred: مشکل حساب!")
+finally:
+    input("Press Enter to close the browser...") 
+    driver.quit()
